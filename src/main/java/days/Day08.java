c06625e410d78c2a8c9025faec8b689fc5f15f94
YOhonour <1083775096@qq.com>
2021-11-16 14:41:09 +0800

    3

--- src/main/java/days/Day08.java
+++ src/main/java/days/Day08.java
@@ -59,6 +59,33 @@ public class Day08 {
         }
         return max;
     }
+    //用状态数组存
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (dp[i-1] <= 0) dp[i] = nums[i];
+            else dp[i] = (dp[i-1] + nums[i]);
+        }
+        int max = dp[0];
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i] > max) max = dp[i];
+        }
+        return max;
+    }
+    //用两个变量存，结果好像差不多
+    public int maxSubArray2(int[] nums) {
+        int dp_n_1 = nums[0],dp_n;
+        int result = dp_n_1;
+        for (int i = 1; i < nums.length; i++) {
+            if (dp_n_1 <= 0) dp_n = nums[i];
+            else dp_n = (dp_n_1 + nums[i]);
+            if (result < dp_n) result = dp_n;
+            dp_n_1 = dp_n;
+        }
+        return result;
+    }
+    
     public static void main(String[] args) {
         System.out.println(new Day08().fib(5));
     }
