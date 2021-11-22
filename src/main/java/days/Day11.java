33208600731d5e20d37fcef9a304a05b129cb8fb
YOhonour <1083775096@qq.com>
2021-11-22 19:22:18 +0800

    11

--- src/main/java/days/Day11.java
+++ src/main/java/days/Day11.java
@@ -2,6 +2,36 @@ package days;
 
 public class Day11 {
     public ListNode deleteNode(ListNode head, int val) {
-        return null;
+        ListNode ptr=head.next==null ? null :head.next;
+        ListNode last = head;
+        if (val==head.val) return head.next;
+        if (ptr != null && ptr.val==val){
+            head.next = head.next.next;
+            return head;
+        }
+        while (ptr!=null){
+            if (ptr.val==val){
+                last.next = ptr.next;
+                break;
+            }else {
+                last = ptr;
+                ptr = ptr.next;
+            }
+        }
+        return head;
+    }
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        //第一轮，获取链表长度；
+        ListNode ptr = head;
+        int len = 0;
+        while (ptr!=null){
+            len++;
+            ptr = ptr.next;
+        }
+        ptr = head;
+        for (int i = 1; i < len-k-1; i++) {
+            ptr = ptr.next;
+        }
+        return ptr;
     }
 }
