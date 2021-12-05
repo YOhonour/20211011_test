bf2438f5fba2945f7f3ecc922f881b3a39d4fe93
YOhonour <1083775096@qq.com>
2021-12-05 16:30:17 +0800

    16

--- src/main/java/days/Day17.java
+++ src/main/java/days/Day17.java
@@ -1,6 +1,8 @@
 package days;
 
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
 
 public class Day17 {
     public void fastSort(int[] arr){
@@ -43,11 +45,71 @@ public class Day17 {
         }
         return result;
     }
-
+    private void quickSort(int[] arr, int l, int r) {
+        // 子数组长度为 1 时终止递归
+        if (l >= r) return;
+        // 哨兵划分操作（以 arr[l] 作为基准数）
+        int i = l, j = r;
+        while (i < j) {
+            while (i < j && arr[j] >= arr[l]) j--;
+            while (i < j && arr[i] <= arr[l]) i++;
+            swap(arr, i, j);
+        }
+        swap(arr, i, l);
+        // 递归左（右）子数组执行哨兵划分
+        quickSort(arr, l, i - 1);
+        quickSort(arr, i + 1, r);
+    }
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
     public static void main(String[] args) {
-        int[] arr = {2,4,63,1,7,3,34,2,23,14};
+        long between = 0;
         Day17 day17 = new Day17();
-        day17.fastSort(arr);
-        System.out.println(Arrays.toString(arr));
+        //自定义的排序算法
+        int[][] ints1 = day17.generate(10000, 5000);
+        Date bg1 = new Date();
+        for (int i = 0; i < ints1.length; i++) {
+            day17.fastSort(ints1[i]);
+        }
+        Date end1 = new Date();
+        between = end1.getTime()-bg1.getTime();
+        System.out.println(between);//输出时间差
+
+        int[][] ints2 = day17.generate(10000, 5000);
+        bg1 = new Date();
+        for (int i = 0; i < ints1.length; i++) {
+            Arrays.sort(ints2[i]);
+        }
+        end1 = new Date();
+        between = end1.getTime()-bg1.getTime();
+        System.out.println(between);//输出时间差
+
+        int[][] ints3 = day17.generate(10000, 5000);
+        bg1 = new Date();
+        for (int i = 0; i < ints1.length; i++) {
+            day17.quickSort(ints3[i],0,ints3[i].length-1);
+        }
+        end1 = new Date();
+        between = end1.getTime()-bg1.getTime();
+        System.out.println(between);//输出时间差
+    }
+    public int[][] generate(int row,int cl){
+        int[][] arr2 = new int[row][cl];
+        Random random = new Random();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < cl; j++) {
+
+                arr2[i][j] = random.nextInt(500);
+            }
+        }
+        return arr2;
+    }
+    public void printArr(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i+": "+Arrays.toString(arr[i]));
+        }
     }
 }
